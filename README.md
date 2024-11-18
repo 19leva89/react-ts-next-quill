0097e7f7a4b79faa3200e0b00f326734cdd815ac
Dmitry Soboliev <19leva89@gmail.com>
2024-11-18 12:41:11 +0200

    add shadcn components

---
+++ README.md
@@ -0,0 +1,27 @@
+Core Technologies:
+- React 18
+- TypeScript
+- Next 15 (framework)
+
+Data Fetching and State Management:
+- Prisma 5 (ORM for DB)
+
+Form and Validation:
+- React Hook Form (working with forms)
+- Zod (first schema validation)
+
+Middleware and Server Utilities:
+- Concurrently (all projects are running in tandem)
+
+Payment:
+- Stripe (payment service provider)
+
+Styling and UI Frameworks:
+- Lucide React (stylization)
+- shadcn/ui (stylization)
+- Tailwind CSS (stylization)
+- Tailwind CSS Animate
+- Tailwind CSS Typography
+
+Utilities and Libraries:
+- PostCSS (transforms CSS code to AST)
---
+++ src/components/ui/index.ts
@@ -0,0 +1,24 @@
+export { Avatar, AvatarFallback, AvatarImage } from './avatar'
+export { Button } from './button'
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './card'
+export { Dialog, DialogContent, DialogTitle } from './dialog'
+export {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
+} from './dropdown-menu'
+export { Input } from './input'
+export { Progress } from './progress'
+export { Textarea } from './textarea'
+export {
+	Toast,
+	ToastAction,
+	ToastClose,
+	ToastDescription,
+	ToastProvider,
+	ToastTitle,
+	ToastViewport,
+} from './toast'
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip'
